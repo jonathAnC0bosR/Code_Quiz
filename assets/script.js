c5495af519629c5cb673d313ea0aab0e43b751aa
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-10 21:39:47 -0600

    Added final logic

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,12 @@ var wrongAnswer = document.querySelector(".wrong");
 var score = parseInt(0);
 var totalScore = document.querySelector(".total-score");
 var finalPage = document.querySelector(".end-page");
+var intervalId;
+var submitButton = document.querySelector('.submit');
+var highscores = document.querySelector('.highscores');
+var highscoresPage = document.querySelector('.highscores-page');
+var highscoresButton = document.querySelector('.hg-button');
+var goBackButton = document.querySelector('.go-back');
 
 function startGame() {
     startPage.style.display = 'none';
@@ -26,14 +32,15 @@ function startGame() {
     // Is this necessary?
     setTimer();
     questions();
+    timer.textContent = '60'
 }
 
 function setTimer() {   
-    var intervaId = setInterval(function() {
+     intervalId = setInterval(function() {
         timer.textContent = secondsLeft;
         if(--secondsLeft <= 0) {
             // Why we have to clear the interval?
-            clearInterval(intervaId);
+            clearInterval(intervalId);
             endGame();
         }
     }, 1000);
@@ -46,89 +53,121 @@ function endGame() {
     secondQuestion.style.display = 'none';
     thirdQuestion.style.display = 'none';
     fourthQuestion.style.display = 'none';
-    correctAnswer.style.display = 'none';
-    wrongAnswer.style.display = 'none';
     totalScore.textContent = score;
 }
 
 function playAgain() {
     finalPage.style.display = 'none';
     startPage.style.display = 'block';
+    correctAnswer.style.display = 'none';
+    wrongAnswer.style.display = 'none';
     secondsLeft = parseInt(59);
+    clearInterval(intervalId);
     score = parseInt(0);
 }
 
 function questions() {
     startPage.style.display = 'none';
     firstQuestion.style.display = 'block';
-    firstAnswer.forEach(answer => {
-        answer.addEventListener('click', () => {
-            if (answer.dataset.correct === 'true') {
-                correctAnswer.style.display = 'block';
-                wrongAnswer.style.display = 'none';
-                secondsLeft += 10;
-                score += 11;
-                secondQuestion.style.display = 'block';
-            } else {
-                wrongAnswer.style.display = 'block';
-                correctAnswer.style.display = 'none';
-                secondsLeft -= 10;
-                secondQuestion.style.display = 'block';
-            }
-        })
+}
+
+firstAnswer.forEach(answer => {
+    answer.addEventListener('click', () => {
+        if (answer.dataset.correct === 'true') {
+            correctAnswer.style.display = 'block';
+            wrongAnswer.style.display = 'none';
+            secondsLeft += 10;
+            score += 11;
+            secondQuestion.style.display = 'block';
+        } else {
+            wrongAnswer.style.display = 'block';
+            correctAnswer.style.display = 'none';
+            secondsLeft -= 10;
+            secondQuestion.style.display = 'block';
+        }
     })
-    secondAnswer.forEach(answer => {
-        answer.addEventListener('click', () => {
-            if(answer.dataset.correct === 'true') {
-                correctAnswer.style.display = 'block';
-                wrongAnswer.style.display = 'none';
-                secondsLeft += 10;
-                score += 11;
-                thirdQuestion.style.display = 'block';
-            } else {
-                wrongAnswer.style.display = 'block';
-                correctAnswer.style.display = 'none';
-                secondsLeft -= 10;
-                thirdQuestion.style.display = 'block';
-            }
-        })
+})
+secondAnswer.forEach(answer => {
+    answer.addEventListener('click', () => {
+        if(answer.dataset.correct === 'true') {
+            correctAnswer.style.display = 'block';
+            wrongAnswer.style.display = 'none';
+            secondsLeft += 10;
+            score += 11;
+            thirdQuestion.style.display = 'block';
+        } else {
+            wrongAnswer.style.display = 'block';
+            correctAnswer.style.display = 'none';
+            secondsLeft -= 10;
+            thirdQuestion.style.display = 'block';
+        }
     })
-    thirdAnswer.forEach(answer => {
-        answer.addEventListener('click', () => {
-            if(answer.dataset.correct === 'true') {
-                correctAnswer.style.display = 'block';
-                wrongAnswer.style.display = 'none';
-                secondsLeft += 10;
-                score += 11;
-                fourthQuestion.style.display = 'block';
-            } else {
-                wrongAnswer.style.display = 'block';
-                correctAnswer.style.display = 'none';
-                secondsLeft -= 10;
-                fourthQuestion.style.display = 'block';
-            }
-        })
+})
+thirdAnswer.forEach(answer => {
+    answer.addEventListener('click', () => {
+        if(answer.dataset.correct === 'true') {
+            correctAnswer.style.display = 'block';
+            wrongAnswer.style.display = 'none';
+            secondsLeft += 10;
+            score += 11;
+            fourthQuestion.style.display = 'block';
+        } else {
+            wrongAnswer.style.display = 'block';
+            correctAnswer.style.display = 'none';
+            secondsLeft -= 10;
+            fourthQuestion.style.display = 'block';
+        }
     })
-    fourthAnswer.forEach(answer => {
-        answer.addEventListener('click', () => {
-            if(answer.dataset.correct === 'true'){
-                correctAnswer.style.display = 'block';
-                wrongAnswer.style.display = 'none';
-                secondsLeft += 10;
-                score += 11;
-                endGame();
-            } else {
-                wrongAnswer.style.display = 'block';
-                correctAnswer.style.display = 'none';
-                secondsLeft -= 10;
-                endGame();
-            }
-        })
+})
+fourthAnswer.forEach(answer => {
+    answer.addEventListener('click', () => {
+        if(answer.dataset.correct === 'true'){
+            correctAnswer.style.display = 'block';
+            wrongAnswer.style.display = 'none';
+            secondsLeft += 10;
+            score += 11;
+            endGame();
+        } else {
+            wrongAnswer.style.display = 'block';
+            correctAnswer.style.display = 'none';
+            secondsLeft -= 10;
+            endGame();
+        }
     })
+})
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    var initials = document.querySelector('.form_field').value;
+    var storedInitials = JSON.parse(localStorage.getItem('initials'));
+    if (!storedInitials) {
+        var arrayInitials = [{"initials": initials, "score": score}]
+        localStorage.setItem('initials', JSON.stringify(arrayInitials));
+    } else {
+        storedInitials.push({"initials": initials, "score": score});
+        localStorage.setItem('initials', JSON.stringify(storedInitials));
+    }
+    renderScores();
+    finalPage.style.display = 'none';
+    highscoresPage.style.display = 'block';
+    wrongAnswer.style.display = 'none';
+    correctAnswer.style.display = 'none';
+})
+
+function renderScores () {
+    var info = JSON.parse(localStorage.getItem('initials'));
+    for (var i=0; i < info.length; i++) {
+        var listScore = document.createElement('li');
+        listScore.textContent = "Name:  "  + info[i].initials + " Score: " + info[i].score;
+        highscores.appendChild(listScore);
+    }
 }
 
 
-
+goBackButton.addEventListener('click', function() {
+    highscoresPage.style.display = 'none';
+    startPage.style.display = 'block';
+})
 startButton.addEventListener("click", startGame);
 playAgainButton.addEventListener("click", playAgain);
 
